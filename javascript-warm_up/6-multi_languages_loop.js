7c709f72bd8e0acb15644c46667e041a1c3332c7
abel <ab.bell205@gmail.com>
2023-09-06 11:00:42 +0100

    initial commit for task 2

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+/*
+A simple Js that iterate using a loop
+*/
+
+const myVar = ['C is fun', 'Python is cool','JavaScript is amazing'];
+
+for(let i = 0; i < myVar.length; i++){
+    console.log(myVar[i]);
+}
