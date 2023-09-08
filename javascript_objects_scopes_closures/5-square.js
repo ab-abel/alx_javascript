d58b4e236b4a6789288e8367994b8c2d9f7ef9c9
ab-abel <ab.bell205@gmail.com>
2023-09-08 23:40:02 +0100

    initial commit for task 5

---
+++ javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const Rectangle = require("./0-rectangle");
+
+class Square extends Rectangle {
+    constructor(size){
+        super(size, size);
+        this.size = size;
+    }
+}
