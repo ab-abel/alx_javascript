f0f802cc53d07b287265cf328dcc67b0503058aa
ab-abel <ab.bell205@gmail.com>
2023-09-08 20:25:52 +0100

    added other conditions

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -3,7 +3,7 @@
 
 module.exports = class Rectangle {
     constructor(w, h) {
-        if(w <= 0 || h <= 0) {
+        if(w <= 0 || h <= 0 || h == undefined || w == undefined) {
             return null;
         } else {
             this.width = w;
