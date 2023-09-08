093d28ee431ee7d03cec9d60eab6af26ee15f354
ab-abel <ab.bell205@gmail.com>
2023-09-08 22:04:14 +0100

    fixing issues with *

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -16,9 +16,9 @@ module.exports = class Rectangle {
     print() {
         for (let i = 0; i < this.height; i++) {
             for (let j = 0; j < this.width; j++) {
-                console.log('*');
+                print('*');
             };
-            console.log('');
+            println("");
         };
     }
 }
