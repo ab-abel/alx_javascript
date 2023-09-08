020a0a73bd728807b6e82319814c092c6ac015e4
ab-abel <ab.bell205@gmail.com>
2023-09-08 23:42:18 +0100

    added module export

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const Rectangle = require("./0-rectangle");
+const Rectangle = require("./4-rectangle");
 
-class Square extends Rectangle {
+module.exports = class Square extends Rectangle {
     constructor(size){
         super(size, size);
         this.size = size;
