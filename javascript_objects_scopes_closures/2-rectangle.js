173514d61443d40d98565cd48d1d07b789766b44
ab-abel <ab.bell205@gmail.com>
2023-09-08 20:19:01 +0100

    initial commit for task 2

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,12 @@
+// this code return nul if w or w <=0
+
+module.exports = class Rectangle {
+    constructor(w, h) {
+        if(w <= 0 || h <= 0) {
+            return null;
+        } else {
+            this.width = w;
+            this.height = h;
+        }
+    }
+}
