2c76fdef90afb34603f9d8655307cc17dd7f8577
ab-abel <ab.bell205@gmail.com>
2023-09-12 21:16:01 +0100

    added encoding

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -7,4 +7,5 @@ const path_to_store = process.agrv[3];
 const request = require('request');
 const fs = require('fs');
 
-request.get(url).pipe(fs.createWriteStream(path_to_store));
+request.get(url, {encoding:'utf-8'})
+    .pipe(fs.createWriteStream(path_to_store));
