f50f69128e22201fcffa1c2781f6afe33efa26c9
ab-abel <ab.bell205@gmail.com>
2023-09-13 18:47:14 +0100

    task 2 correction

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -1,28 +1,19 @@
-const req = require('request');
+const request = require('request');
 
 const url = 'https://swapi-api.alx-tools.com/api/films/';
 
-const user = 'https://swapi-api.alx-tools.com/api/people/18/'
-
-let lists = [];
-
-req.get(url, (error, response, body) => {
-    const results = JSON.parse(body);
-    results.results.forEach(element => {
-        // lists.push(element.characters.filter(new_char => new_char === user));
-        lists.push(element.characters);
-    });
-    // console.log(lists);
-    const nw_list = [];
-    lists.forEach((people) => {
-        people.forEach((person) =>{
-            // console.log(person)
-            if(person === user){
-                nw_list.push(person)
-            };
+request.get(url, (error, response, body) =>{
+    if(!error && response.statusCode == 200){
+        const films = JSON.parse(body);
+        let counter = 0;
+        films.results.forEach((film) =>{
+            film.characters.forEach((actor) => {
+                const split_film = actor.split('/');
+                if(split_film[split_film.length-2] == 18) {
+                    counter += 1;
+                }
+            })
         })
-    });
-
-    console.log(nw_list.length)
+        console.log(counter);
+    }
 });
-
--- javascript-network/test.js
+++ javascript-network/test.js
@@ -1,17 +1,34 @@
-const request = require('request');
-
-const url = process.argv[2];
-
-request.get(url)
-    .on('response', response=>{
-        console.log(`code: ${response.statusCode}`);
-    })
+str = 'goo/to/cho/1/'
+const val = str.split('/')
+console.log(val.length)
+console.log(val[val.length-2])
 
 
-const countBy = (arr, fn) =>
-    arr.map(typeof fn === 'function' ? fn : val => val[fn])
-    .reduce((acc, val) => {
-        acc[val] = (acc[val] || 0) + 1;
-        return acc;
-    },{});
-        
+const req = require('request');
+
+const url = 'https://swapi-api.alx-tools.com/api/films/';
+
+const user = 'https://swapi-api.alx-tools.com/api/people/18/'
+
+let lists = [];
+
+req.get(url, (error, response, body) => {
+    const results = JSON.parse(body);
+    results.results.forEach(element => {
+        // lists.push(element.characters.filter(new_char => new_char === user));
+        lists.push(element.characters);
+    });
+    // console.log(lists);
+    const nw_list = [];
+    lists.forEach((people) => {
+        people.forEach((person) =>{
+            // console.log(person)
+            if(person === user){
+                nw_list.push(person)
+            };
+        })
+    });
+
+    console.log(nw_list.length)
+});
+
