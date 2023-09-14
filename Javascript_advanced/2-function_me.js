c3f568b7232e091959749ebf0c26224671ce0bac
ab-abel <ab.bell205@gmail.com>
2023-09-14 11:51:51 +0100

    intial commit for task 2

---
+++ Javascript_advanced/2-function_me.js
@@ -0,0 +1,11 @@
+// function is aimed to tst closure function
+/**
+ * @param {string} fullName 
+ */
+function welcomeMessage(fullName) {
+    alert(`Welcome ${fullName}`);
+}
+
+let guillaume = welcomeMessage('Guillaume');
+let alex = welcomeMessage('Alex');
+let fred = welcomeMessage('Fred');
