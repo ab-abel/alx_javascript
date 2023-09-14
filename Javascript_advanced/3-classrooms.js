25b2dff8c51ca8f3752bae3642db77eb716a1c38
ab-abel <ab.bell205@gmail.com>
2023-09-14 11:52:06 +0100

    intitial commit for task 3

---
+++ Javascript_advanced/3-classrooms.js
@@ -0,0 +1,30 @@
+/**
+ * 
+ * @param {Int} numbersOfStudents 
+ */
+
+function createClassRoom(numbersOfStudents) {
+    /**
+     * 
+     * @param {Int} seat 
+     */
+    function studentSeat(seat) {
+        // return a funtion that return seat
+        return function(){
+            return seat;
+        }; 
+    }
+
+    // create an empty array of students
+    let students = [];
+
+    // loop through the number of student
+    for(let i = 0; i <= numbersOfStudents; i++) {
+
+        // add the number of seat to student array
+        students.push(studentSeat(i + 1));
+    }
+    return students;
+}
+
+const classRoom = createClassRoom(10);
