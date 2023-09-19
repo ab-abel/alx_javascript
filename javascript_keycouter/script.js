1bde1fbf9d36b22bc161a6d6bf7f838547328930
ab-abel <ab.bell205@gmail.com>
2023-09-19 20:31:10 +0100

    count anykey enter

--- javascript_keycouter/script.js
+++ javascript_keycouter/script.js
@@ -1,12 +1,16 @@
-const textArea = document.getElementById('text-area');
+const textArea = document.getElementById('charter-field');
 
 let counter = 0;
 
-const ignoreKey = ['Backspace', 'Shift', 'Control','CapsLock', 'Enter', 'Alt'];
+const ignoreKey = ['Backspace', 'Shift', 'Control', 'CapsLock', 'Enter', 'Alt'];
 
 textArea.onkeydown = function(event) {
     const output = document.getElementById('display');
     let keyStroke = event.key;
 
-    if(!ignoreKey  )
+    // if(!ignoreKey  
+    if(keyStroke) {
+        output.textContent = `the button is pressed ${counter} times`
+    }
+    counter++;
 }
