ae4e8b2058ca42a33ae8e7ce0cfc052292b7b1bf
ab-abel <ab.bell205@gmail.com>
2023-09-29 00:55:29 +0100

    added doc strings

--- html_js_validation/0-password-strength.js
+++ html_js_validation/0-password-strength.js
@@ -1,23 +1,27 @@
+// get tags from HTML
 const passwordE1 = document.getElementById('password');
 const btn = document.querySelector('button');
 
+// listen to btn click event
  btn.addEventListener('click', (e)=>{
     e.preventDefault();
     validatePassword();
  });
 
 
- const isRequired = value => value === '' ? false:true;
-
-const isShort = (length, min) => length < min ? false : true;
-
-const isLong = (length, max) => length > max ? false : true;
-
+/**
+ *@function validate password
+ *@returns {Boolean}
+ */
 const validatePassword = () => {
+    
+    // define variables
     let valid = false;
 
+    // escape password values
     const password  =  passwordE1.value.trim();
-    console.log(password);
+    
+    // check using helper fcn
     if(!isRequired(password)){
         showError('password cannot be empty');
     }else if(!isShort(password.length, 8)) {
@@ -32,37 +36,68 @@ const validatePassword = () => {
         showError('Password must contain atleast one numeric digit')
     }else if(!isSpecialCharacters(password)) {
         showError('Password must contain atleast one special character')
+    
     }else{
+
+        // only run if the test pass
         valid = true;
     }
 
     return valid;
 }
 
+/**
+ * @function ShowError 
+ * @param {String} error 
+ */
 const showError  = (error) => {
-
     const htmlField = document.getElementById('error');
-    // htmlField.textContent = '';
     htmlField.textContent = error;
 
 }
 
 
+/*** Helper Functions ***/
+
+const isRequired = value => value === '' ? false:true;
+const isShort = (length, min) => length < min ? false : true;
+const isLong = (length, max) => length > max ? false : true;
+
+/**
+ * @function isLowercase
+ * @param {String} password 
+ * @returns 
+ */
 const isLowercase = (password) => {
     const re = new RegExp("^(?=.*[a-z])");
     return re.test(password);
 }
 
+/**
+ * @function isUppercase
+ * @param {String} password
+ * @returns 
+ */
 const isUppercase = (password) => {
     const re = new RegExp("^(?=.*[A-Z])");yh;
     return re.test(password);
 }
 
+/**
+ * @function isNumeric
+ * @param {String} password 
+ * @returns 
+ */
 const isNumeric = (password) => {
     const re = new RegExp("^(?=.*[0-9])");
     return re.test(password);
 }
 
+/**
+ * @function isSpecialCharacters
+ * @param {String} password 
+ * @returns 
+ */
 const isSpecialCharacters = (password) => {
     const re = new RegExp("^(?=.*[!@#\$%\^&\*])");
     return re.test(password);
--- html_js_validation/1-email-validation.js
+++ html_js_validation/1-email-validation.js
@@ -1,18 +1,28 @@
+// get element from the HTML page
 const emailE1 = document.getElementById('email');
 const btn = document.querySelector('button');
 
+// create a btn listening event
  btn.addEventListener('click', (e)=>{
     e.preventDefault();
     validateEmail();
  });
 
-const isRequired = value => value === '' ? false : true;
 
+
+/**
+ * @function validateEmail
+ * @returns {Boolean}
+ */
 const validateEmail = () => {
 
+    // defines variable
     let valid = false;
 
+    // escape the inputed strings
     const email  =  emailE1.value.trim();
+
+    // vaidation check
     if(!isRequired(email)){
         showError('email cannot be empty');
     }else if(!isEmailValid(email)) {
@@ -24,18 +34,36 @@ const validateEmail = () => {
     return valid;
 }
 
+/**
+ * @function ShowError
+ * @param {String} error 
+ * @returns {null}
+ */
 const showError  = (error) => {
     const htmlField = document.getElementById('error');
     htmlField.textContent = error;
     htmlField.style.color ='red';
 }
 
+/**
+ * @function showSucess
+ * @param {String} error 
+ * @returns {null}
+ */
 const showSucess = (success) => {
     const htmlField = document.getElementById('error');
     htmlField.textContent = success;
     htmlField.style.color = 'green';
 }
 
+/***** Helper Functions ******/
+const isRequired = value => value === '' ? false : true;
+
+/**
+ * @function isEmailValid
+ * @param {String} email 
+ * @returns {Boolean}
+ */
 const isEmailValid = (email) => {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
